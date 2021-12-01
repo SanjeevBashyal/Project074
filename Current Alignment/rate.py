ee32ea926df759ccde39fdf6c3be4741b2c60741
Sandip Dahal <badafest1765@gmail.com>
2021-12-01 11:36:46 +0545

    scripts push

--- Current Alignment/rate.py
+++ Current Alignment/rate.py
@@ -1,38 +1,54 @@
 import csv, sys, json
 from math import sqrt
+from rateSteps import rateStep
 
-def statAnal(data):
+def statAnal(data, cutHigh = False):
 	data.sort()
+	dataLen = len(data)
+	
+	thirdQIndex = 3*(dataLen + 1)/4 - 1
+	thirdQuartile = data[int(thirdQIndex)]+(thirdQIndex - int(thirdQIndex))*(data[int(thirdQIndex)+1] - data[int(thirdQIndex)])
+	
+	if cutHigh:
+		data = list(filter(lambda x: x <= thirdQuartile, data))
+		dataLen = len(data)
+		thirdQIndex = 3*(dataLen + 1)/4 - 1
+		thirdQuartile = data[int(thirdQIndex)]+(thirdQIndex - int(thirdQIndex))*(data[int(thirdQIndex)+1] - data[int(thirdQIndex)])
+		
+	
+	firstQIndex = (dataLen + 1)/4 - 1
+	firstQuartile = data[int(firstQIndex)]+(firstQIndex - int(firstQIndex))*(data[int(firstQIndex)+1] - data[int(firstQIndex)])
+	
+	medQIndex = (dataLen + 1)/2 - 1
+	median = data[int(medQIndex)]+(medQIndex - int(medQIndex))*(data[int(medQIndex)+1] - data[int(medQIndex)])
+	
 	minimum = data[0]
 	maximum = data[-1]
-	dataLen = len(data)
-	quartile = (maximum - minimum)/3
+	quartile = (maximum - minimum)/4
 	frequency = [0,0,0,0]
 	sumData = 0
 	sumData2 = 0
+	
 	for x in data:
-		qIndex = (x - minimum)/quartile
-		frequency[int(qIndex)] += 100/dataLen
+		qIndex = int((x - minimum)/quartile)
+		frequency[min([qIndex,3])] += 100/dataLen
 		sumData += x
 		sumData2 += x**2
-	frequency = {
-		f'{round(minimum,3)} - {round(minimum + quartile,3)}' : round(frequency[0],3),
-		f'{round(minimum + quartile,3)} - {round(minimum + 2 * quartile,3)}' : round(frequency[1],3),
-		f'{round(minimum + 2 * quartile,3)} - {round(maximum - quartile,3)}' : round(frequency[2],3),
-		f'{round(maximum - quartile,3)} - {round(maximum,3)}' : round(frequency[3],3),
-	}
+		
 	average = sumData/dataLen
 	sumData2 = sumData2 - dataLen*average**2
 	sigma2 = sumData2/dataLen
 	sigma = sqrt(sigma2)
 	sd2 = sumData2/(dataLen-1)
 	sd = sqrt(sd2)
-	firstQIndex = (dataLen + 1)/4 - 1
-	medQIndex = (dataLen + 1)/2 - 1
-	thirdQIndex = 3*(dataLen + 1)/4 - 1
-	firstQuartile = data[int(firstQIndex)]+(firstQIndex - int(firstQIndex))*(data[int(firstQIndex)+1] - data[int(firstQIndex)])
-	median = data[int(medQIndex)]+(medQIndex - int(medQIndex))*(data[int(medQIndex)+1] - data[int(medQIndex)])
-	thirdQuartile = data[int(thirdQIndex)]+(thirdQIndex - int(thirdQIndex))*(data[int(thirdQIndex)+1] - data[int(thirdQIndex)])
+	
+	frequency = {
+		f'{round(minimum,3)} - {round(minimum + quartile,3)}' : round(frequency[0],3),
+		f'{round(minimum + quartile,3)} - {round(minimum + 2 * quartile,3)}' : round(frequency[1],3),
+		f'{round(maximum - 2 * quartile,3)} - {round(maximum - quartile,3)}' : round(frequency[2],3),
+		f'{round(maximum - quartile,3)} - {round(maximum,3)}' : round(frequency[3],3),
+	}
+	
 	return {
 		'N': dataLen,
 		'sum': round(sumData,3),
@@ -71,7 +87,7 @@ with open(points) as pointFile:
 		hzAngles.append(round(float(row['hz. angle']),3))
 
 bridgeAnal = statAnal(bridges)
-gradeAnal = statAnal(grades)
+gradeAnal = statAnal(grades, True)
 hzAnglesAnal = statAnal(hzAngles)
 
 outputJSON = {
@@ -80,7 +96,30 @@ outputJSON = {
 	'Grades': gradeAnal,
 	'Hz Angles': hzAnglesAnal
 }
+
+def rateAnal(stats, sum = 0, N = 0, med = 0, thirdQ = 0, max = 0, avg = 0, freq = 1):
+	rateDict = {'sum': sum, 'N': N, 'med': med, 'thirdQ': thirdQ, 'max': max, 'avg': avg, 'freq': freq}
+	for x in ['sum', 'N', 'med', 'thirdQ', 'max', 'avg']:
+		if rateDict[x]!=0:
+			rateDict[x] = min([round(stats[x]/rateDict[x],1),10])
+	if rateDict['freq']!=0:
+		rateDict['freq'] = 0
+		statsFreq = list(stats['freq'].values())
+		for x in range(0,4):
+			rateDict['freq'] += round((x+1)*statsFreq[x]/250,1)*5
+	return rateDict
 	
+ratingJSON = {
+	'Length': f"{round((totalLength - 140000)/rateStep['length'], 1)}",
+	'Bridges': rateAnal(bridgeAnal, **rateStep['bridge']),
+	'Grades': rateAnal(gradeAnal, **rateStep['grade']),
+	'Hz Angles': rateAnal(hzAnglesAnal, **rateStep['hzAngle'])
+}
+
 with open(output+'.json', 'w') as outFile:
 	outFile.write(json.dumps(outputJSON, indent=4))
+	outFile.close()	
+	
+with open(output+'Score.json', 'w') as outFile:
+	outFile.write(json.dumps(ratingJSON, indent=4))
 	outFile.close()	
